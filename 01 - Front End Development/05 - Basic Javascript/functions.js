e49c576ca818ed45d5bc087710ad011826de18dd
Johanny Mateo <jomateo@users.noreply.github.com>
2017-07-18 14:23:23 -0400

    functions, global, and local var

---
+++ 01 - Front End Development/05 - Basic Javascript/functions.js
@@ -0,0 +1,29 @@
+// // sums up 2 arguements
+// function functionWithArgs (arg1, arg2) {
+//     console.log(arg1 + arg2);
+// }
+//
+// functionWithArgs (3, 5);
+
+// //Global Scope and Functions
+// // Declare your variable here
+// var myGlobal = 10;
+//
+// function fun1() {
+//   // Assign 5 to oopsGlobal Here
+//   oopsGlobal = 5;
+// }
+//
+// // Only change code above this line
+// function fun2() {
+//   var output = "";
+//   if (typeof myGlobal != "undefined") {
+//     output += "myGlobal: " + myGlobal;
+//   }
+//   if (typeof oopsGlobal != "undefined") {
+//     output += " oopsGlobal: " + oopsGlobal;
+//   }
+//   console.log(output);
+// }
+
+//Local Scope and Functions
